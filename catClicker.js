ba282bc7726072057303b00e19cdcf8de60c8dfc
unknown <rleite@RT450s.ifs.tuwien.ac.at>
2015-06-03 15:40:00 +0200

    - adding that hides elements - still allowing click count on elements

--- catClicker.js
+++ catClicker.js
@@ -4,17 +4,32 @@ var counter = [0,0,0,0,0];
 
 
 
-	function clickCounter(i){
-		console.debug("aaa");
-		
-		counter[i]++;
-
-		//$("#clicker-count").text("VisClicker: "+counter);
-		$("#"+vis[i]).find("h2").textContent = vis[i]+": "+counter[i];
-	};	
-
 	function generateElements(){
 		//console.debug("aaa");
+		generateImages();
+		generateList();		
+	};
+
+
+	function generateList(){
+		
+		var listDiv = $("#main-list");
+		var listElement = document.createElement("ul");
+		
+		for(var i=0; i<vis.length; i++){
+			var li = document.createElement('li');
+			var id = vis[i];
+			li.id = id;
+			li.textContent = id;
+			li.addEventListener('click',turnVisible);
+			listElement.appendChild(li);
+		}
+
+		listDiv.append(listElement); //append() vem do jQuery
+	}
+
+	function generateImages(){
+		var imageList = $("#image-list");
 		for(var i=0; i<vis.length;i++){
 			//Cria div para cada Vis
 			var elem = document.createElement('div');
@@ -32,35 +47,49 @@ var counter = [0,0,0,0,0];
 			//Adiciona funcionalidade
 			img.addEventListener('click',(function(iCopy){
 				return function(){
-					console.debug("aaa");		
+					console.debug("aaa: "+counter[iCopy]);       
 					counter[iCopy]++;
-					$("#"+vis[iCopy]).find("h2").text(vis[iCopy]+": "+counter[iCopy]);
+					imageList.find("#"+vis[iCopy]).find("h2").text(vis[iCopy]+": "+counter[iCopy]);
 				}
-			})(i));
-			
+			})(i));         
 
 			elem.appendChild(img);
-			document.body.appendChild(elem);
+			elem.style.float = "left";//alinha horizontalmente as imagens
+			elem.style.visibility = "hidden";
+			imageList.append(elem);
 		}
-	};
+	}
 
 	function picAddressByName(name){
 		switch(name){
 			case "ScatterPlot":
 				return "plot.png";
-				break;
 			case "BarChart":
 				return "bar.png";
-				break;
 			case "TreeMap":
-			    return "treemap.png";
-				break;
+				return "treemap.png";
 			case "HBE": 
 				return "edge.png";
-				break;
 			case "ParallelCoordinates":
 				return "coor.png";
-				break;
-			default: return "erro.png";
+			default: 
+				return "error.png";
 		}
 	};
+
+	function turnVisible(){
+		
+		var images = $("#image-list"); 
+		//debugger;
+		//always gives you a nodeList object, that's why we need to get the first element '[0]'
+		images.find("#"+this.id)[0].style.visibility = "visible";
+
+		for(var i=0; i<vis.length; i++){
+			if(vis[i]!=this.id){
+				
+				console.debug(vis[i]);
+				images.find("#"+vis[i])[0].style.visibility = "hidden";
+			}//end if
+		}//end for
+
+	}
